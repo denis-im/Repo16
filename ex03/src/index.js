332fdbc279a050275437f22e5742ea6adc9014f9
zabec <denis.imsirovic@gmail.com>
2022-04-12 10:53:46 +0200

    not working

---
+++ ex03/src/index.js
@@ -0,0 +1,15 @@
+// only change code below this line
+class Person {
+  static display() {
+    let message = "Static method is invoked from Person class";
+    return message;
+  }
+
+  show() {
+    console.log(this.display);
+  }
+}
+// Only change code above this line
+var message = new Person();
+message.show();
+module.exports = Person;
